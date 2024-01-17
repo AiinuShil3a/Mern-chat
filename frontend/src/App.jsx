cf93bde16e5d0cdc3841bd47a2faf3c45b2a514f
Supphalak Maneepanpanit <644259034@webmail.npru.ac.th>
2024-01-17 19:59:22 +0700

    v1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { UserContextProvider } from './context/userContext'
 import Routes from './Routes'
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <UserContextProvider>
+      <Navbar />
       <Routes />
     </UserContextProvider>
   )
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-xl">CHAT WITH ME FREE WIFI</div>
+        <div>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default navbar
+export default Navbar;
